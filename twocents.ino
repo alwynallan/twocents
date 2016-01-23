604fc620303dd7c97244596026b498d9f55b322b
Peter Allan <alwynallan@gmail.com>
2016-01-23 12:38:43 -0500

    Added servedata branch for isolation experiment

--- twocents.ino
+++ twocents.ino
@@ -13,20 +13,26 @@ void setup() {
   static uint8_t charge_loops[SAMPLES];
   static uint8_t discharge_loops[SAMPLES];
 
-  for(int i=0; i<SAMPLES; i++){
-    register uint8_t cl=0, dl=0;
+  uint32_t t=ESP.getCycleCount();
+  for(register int i=0; i<SAMPLES; i++){
+    register uint8_t loops=0;
     digitalWrite(D7, HIGH);
-    while(digitalRead(D8) != HIGH) cl++;
-    delayMicroseconds(20); // used for testing but not adopted in twocents()
+    while(digitalRead(D8) != HIGH) loops++;
+    charge_loops[i] = loops;
+    //delayMicroseconds(20); // used for testing but not adopted in twocents()
+    // 262 ms with delay above, 198,781 bit/s entropy
+    // 117 ms without delay,    376,677 bit/s entropy
+    loops=0;
     digitalWrite(D7, LOW);
-    while(digitalRead(D8) != LOW) dl++;
-    charge_loops[i] = cl;
-    discharge_loops[i] = dl;
+    while(digitalRead(D8) != LOW) loops++;
+    discharge_loops[i] = loops;
   }
+  t = (ESP.getCycleCount() - t)/80; // us
 
   for(int i=0; i<SAMPLES; i++){
-    Serial.println(String(charge_loops[i]) + " " + String(discharge_loops[i]));
+    Serial.println(String(charge_loops[i]) + '\t' + String(discharge_loops[i]));
   }
+  Serial.println(String(SAMPLES) + " samples in " + String(t) + "us");
   //--------------------- end of test -----------------------------------------
 }
 
@@ -61,4 +67,5 @@ void loop() {
     //Serial.print(t);
   }
   Serial.println();
+  while(1) yield();
 }
